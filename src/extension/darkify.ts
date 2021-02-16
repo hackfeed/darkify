7cfb8ca6e9bfd05c046901d18b3ca9b9d724ff79
Sergey Kononenko <hackfeed@yandex.ru>
2021-02-17 02:46:31 +0300

    Update custom vars fetching

--- src/extension/darkify.ts
+++ src/extension/darkify.ts
@@ -21,7 +21,7 @@ const printHSL = (color: HSL) => {
   return `hsl(${color.hue}, ${color.saturation}%, ${color.lightness}%)`;
 };
 
-const stringToRgb = (color: string) => {
+const RGBstringToRGB = (color: string) => {
   const colors = color
     .slice(4, color.length - 1)
     .split(",")
@@ -30,6 +30,31 @@ const stringToRgb = (color: string) => {
     red: colors[0],
     green: colors[1],
     blue: colors[2],
+  };
+
+  return convColor;
+};
+
+const RGBAstringToRGB = (color: string) => {
+  const colors = color
+    .slice(5, color.length - 1)
+    .split(",")
+    .map((c) => Number(c.trim()));
+  const convColor: RGB = {
+    red: colors[0],
+    green: colors[1],
+    blue: colors[2],
+  };
+
+  return convColor;
+};
+
+const HEXstringToRGB = (color: string) => {
+  const bigInt = parseInt(color.slice(1, color.length), 16);
+  const convColor: RGB = {
+    red: (bigInt >> 16) & 255,
+    green: (bigInt >> 8) & 255,
+    blue: bigInt & 255,
   };
 
   return convColor;
@@ -89,14 +114,6 @@ const getRelativeLuminance = (color: RGB): number => {
   return relLum;
 };
 
-const getStyle = (el: HTMLElement, prop: string) => {
-  const style = (window.getComputedStyle
-    ? window.getComputedStyle(el).getPropertyValue(prop)
-    : el.style[prop.replace(/-([a-z])/g, (g: string) => g[1].toUpperCase())]) as string;
-
-  return style;
-};
-
 const getDominantColor = async (): Promise<RGB> => {
   const canvas: HTMLCanvasElement = await html2canvas(document.body);
   const dataUrl = canvas.toDataURL();
@@ -147,23 +164,62 @@ const main = async () => {
             }
           } else if (key.toLowerCase().includes("color")) {
             if (styleRule[key]) {
-              const initColor = stringToRgb(styleRule[key]);
+              let initColorRGB: RGB;
+              const color: string = styleRule[key];
 
-              let initLum = getRelativeLuminance(initColor);
-              let backLum = getRelativeLuminance(BASE_COLOR);
-              if (initLum > backLum) {
-                [backLum, initLum] = [initLum, backLum];
+              // if (styleRule[key].startsWith("var")) {
+              //   const prop = styleRule[key].slice(4, styleRule[key].length - 1);
+              //   color = window.getComputedStyle(document.body).getPropertyValue(prop);
+              // } else {
+              //   color = styleRule[key];
+              // }
+
+              if (color.startsWith("#")) {
+                initColorRGB = HEXstringToRGB(color);
+              } else if (color.startsWith("rgba")) {
+                initColorRGB = RGBAstringToRGB(color);
+              } else {
+                initColorRGB = RGBstringToRGB(color);
               }
+              const initColorHSL = convertRGBToHSL(initColorRGB);
 
-              const multiplier = (backLum + 0.05) / (initLum + 0.05) + 1;
-              const colorHsl: HSL = {
-                hue: BASE_BACKGROUND_HSL.hue,
-                saturation: BASE_BACKGROUND_HSL.saturation,
-                lightness: multiplier
-                  ? BASE_BACKGROUND_HSL.lightness * multiplier
-                  : BASE_BACKGROUND_HSL.lightness,
-              };
-              styleRule[key] = printHSL(colorHsl);
+              const isCloseToBlue =
+                initColorHSL.hue > 200 &&
+                initColorHSL.hue < 250 &&
+                initColorHSL.saturation > 60 &&
+                initColorHSL.lightness < 60;
+              const isCloseToGreen =
+                initColorHSL.hue > 100 &&
+                initColorHSL.hue < 150 &&
+                initColorHSL.saturation > 60 &&
+                initColorHSL.lightness < 60;
+              const isCloseToRed =
+                (initColorHSL.hue > 340 || initColorHSL.hue < 10) &&
+                initColorHSL.saturation > 60 &&
+                initColorHSL.lightness < 60;
+
+              if (!isCloseToBlue || !isCloseToGreen || !isCloseToRed) {
+                let initLum = getRelativeLuminance(initColorRGB);
+                let backLum = getRelativeLuminance(BASE_COLOR);
+                if (initLum > backLum) {
+                  [backLum, initLum] = [initLum, backLum];
+                }
+
+                const multiplier = (backLum + 0.05) / (initLum + 0.05) + 1;
+                const colorHsl: HSL = {
+                  hue: BASE_BACKGROUND_HSL.hue,
+                  saturation: BASE_BACKGROUND_HSL.saturation,
+                  lightness: multiplier
+                    ? BASE_BACKGROUND_HSL.lightness * multiplier
+                    : BASE_BACKGROUND_HSL.lightness,
+                };
+
+                if (key.toLowerCase().startsWith("border")) {
+                  styleRule["borderColor"] = printHSL(colorHsl);
+                } else {
+                  styleRule[key] = printHSL(colorHsl);
+                }
+              }
             }
           }
         }
